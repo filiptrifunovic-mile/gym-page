ceea2ecd1f680f9820b02242524bc27cfbc54d9a
Filip <filip.trifunovic23@gmail.com>
2023-01-10 17:03:30 +0100

    mobile responsive, navbar condition

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Navbar from '@/scenes/navbar'
 import { useState } from 'react';
 import { SelectedPage } from '@/shared/types';
+import { useEffect } from 'react';
 
 
 
@@ -10,10 +11,26 @@ import { SelectedPage } from '@/shared/types';
 function App() {
 
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
+  const [isTopPage, setIsTopPage] = useState<boolean>(true)
 
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if(window.scrollY === 0) {
+        setIsTopPage(true)
+        setSelectedPage(SelectedPage.Home)
+      }
+
+      if(window.scrollY !== 0) setIsTopPage(false)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  },[]);
+    
 
   return <div className="app bg-gray-20">
-    <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+    <Navbar isTopPage={isTopPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
   </div>;
 }
 
