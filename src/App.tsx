74ea6cb182d98ae734ecfd92ba46be7117b8c1ce
Filip <filip.trifunovic23@gmail.com>
2023-01-10 14:20:17 +0100

    navbar part 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import { useState } from "react";
+import Navbar from '@/scenes/navbar'
+
 
 function App() {
-  return <div className="app">app</div>;
+  return <div className="app bg-gray-20">
+    <Navbar />
+  </div>;
 }
 
 export default App;
---
+++ src/scenes/navbar/index.tsx
@@ -0,0 +1,42 @@
+
+import { useState } from "react"
+import {Bars3Icon, XMarkIcon} from '@heroicons/react/24/solid'
+import Logo from '@/assets/Logo.png'
+
+type Props = {}
+
+const index = (props: Props) => {
+
+  const flexBetween = "flex items-center justify-between"
+
+  return (
+    <nav>
+      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6 `}>
+        <div className={`${flexBetween} mx-auto w-5/6`}>
+          <div className={`${flexBetween} w-full gap-16`}>
+
+            {/*LEFT SIDE  */}
+            <img src={Logo} alt="logo" />
+
+            {/*RIGHT SIDE  */}
+            <div className={`${flexBetween} w-full`}>
+              <div className={`${flexBetween} gap-8 text-sm`}>
+                <p>Home</p>
+                <p>Benifits</p>
+                <p>Out classes</p>
+                <p>Contact us</p>
+              </div>
+              <div className={`${flexBetween} gap-8`}>
+                <p>Sign in</p>
+                <button>Become a member</button>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default index
